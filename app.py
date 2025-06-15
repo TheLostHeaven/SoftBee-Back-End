ea5d4195fd861e36241622ad84f2708d49097cda
Danii <dannyking07@outlook.es>
2025-06-15 14:32:13 -0500

    Actualizar la inicialización de la aplicación y la creación de tablas en la base de datos

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 from flask import Flask, jsonify
 # from src.routers.home import home
-
+from src.routes.question import question_bp
 from config import Config
-from src.database.db import init_db
+from src.database.db import init_app
 
 
 def app_create(testing=False):
     app = Flask(__name__)
+    app.config['DATABASE'] = 'database.db'
     app.config.from_object(Config)
 
     with app.app_context():
-        init_db()
+        init_app(app)
 
     app.config.from_object(Config)
 
     # app.register_blueprint(home)
+    app.register_blueprint(question_bp, url_prefix='/api')
 
     return app
--- index.py
+++ index.py
@@ -1,5 +1,10 @@
 from app import app_create
 
+from src.database.db import get_db, create_all_tables
+db = get_db()
+create_all_tables(db)
+db.commit()
+
 app = app_create()
 
 if __name__ == '__main__':

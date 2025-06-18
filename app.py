b304d05565f7a67bd47284c4cf84be6f0e6e5624
Danii <dannyking07@outlook.es>
2025-06-18 01:31:18 -0500

    Agregar registro del blueprint de usuarios en la aplicación y mejorar la gestión de la variable de puerto en el archivo de inicio

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
 # from src.routers.home import home
 from src.routes.question import question_bp
+from src.routes.users import user_bp
+
 from config import Config
 from src.database.db import init_app, get_db, close_db, init_database
 
@@ -20,6 +22,7 @@ def app_create(testing=False):
 
     # app.register_blueprint(home)
     app.register_blueprint(question_bp, url_prefix='/api')
+    app.register_blueprint(user_bp, url_prefix='/api')
 
 
     return app

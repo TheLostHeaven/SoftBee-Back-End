0259a6e7a5fc60728124d349c67ad5a177f7978b
Danii <dannyking07@outlook.es>
2025-06-21 22:19:38 -0500

    Refactor la inicialización del servicio de correo electrónico y la creación de rutas de autenticación para mejorar la estructura del código y asegurar el uso adecuado de la base de datos.

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from flask_mail import Mail
 from src.utils.email_service import EmailService
 import os
+from src.database.db import get_db
 #from src.routes.users import user_bp
 
 from config import Config
@@ -11,8 +12,7 @@ def create_app(testing=False):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(Config)
     CORS(app)
-    mail = Mail(app)
-    email_service = EmailService(mail)
+
     
     
     from src.database.db import init_app
@@ -25,19 +25,23 @@ def create_app(testing=False):
     from src.routes.users import create_user_routes
     from src.routes.auth import create_auth_routes
 
+    mail = Mail(app)
+    email_service = EmailService(mail)
+
 
     with app.app_context():
-        app.register_blueprint(create_apiary_routes(), url_prefix='/api')
-        app.register_blueprint(create_hive_routes(), url_prefix='/api')
-        app.register_blueprint(create_inventory_routes(), url_prefix='/api')
-        app.register_blueprint(create_question_routes(), url_prefix='/api')
-        app.register_blueprint(create_user_routes(), url_prefix='/api')
-        app.register_blueprint(create_auth_routes(email_service), url_prefix='/api')
+        auth_bp = create_auth_routes(get_db_func=get_db, email_service=email_service)
+        app.register_blueprint(auth_bp, url_prefix='/api')
 
 
     # Registrar blueprints
 
 
+    app.register_blueprint(create_apiary_routes(), url_prefix='/api')
+    app.register_blueprint(create_hive_routes(), url_prefix='/api')
+    app.register_blueprint(create_inventory_routes(), url_prefix='/api')
+    app.register_blueprint(create_question_routes(), url_prefix='/api')
+    app.register_blueprint(create_user_routes(), url_prefix='/api')
 
 
     return app

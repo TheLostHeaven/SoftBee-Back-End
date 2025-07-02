50377453890e558ccbeddff7e4a8e4ec788488c2
Sofia Sanchez <114118697+SoffiaSanchezz@users.noreply.github.com>
2025-07-02 04:26:33 -0500

    Update beehive.py

--- src/routes/beehive.py
+++ src/routes/beehive.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from .controllers.beehive import HiveController
-from src.database.db import get_db
+from .database.db import get_db
 
 def create_hive_routes():
     hive_bp = Blueprint('hive_routes', __name__)
